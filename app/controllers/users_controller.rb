41aedb6af2df5138d9588943fbaa1b60ff04a668
Robert Laws <roblaws@me.com>
2018-03-10 16:57:10 +0300

    update users controller add admin to user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,6 +39,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :username, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :username, :password, :admin)
   end
 end
