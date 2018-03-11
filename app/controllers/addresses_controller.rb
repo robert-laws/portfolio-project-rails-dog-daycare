cf10084f89c72346630c3eb27af6d8ff20046aa2
Robert Laws <roblaws@me.com>
2018-03-11 12:00:21 +0300

    add controller logic to view nested user address data

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,9 +1,21 @@
 class AddressesController < ApplicationController
   def show
-
+    if params[:user_id]
+      @user = User.find(params[:user_id])
+      if current_user != @user
+        redirect_to root_path
+      else
+        @address = @user.addresses.find_by(id: params[:id])
+        if @address.nil?
+          redirect_to user_path(@user), alert: "Address not found"
+        end
+      end
+    else
+      redirect_to root_path
+    end
   end
 
   def edit
-    
+
   end
 end
