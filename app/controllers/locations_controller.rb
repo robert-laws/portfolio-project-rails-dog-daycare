435cb6506f17bbcbd984000551fc9c9a57ed1cfe
Robert Laws <roblaws@me.com>
2018-03-12 21:20:49 +0300

    add basic location controller methods

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -10,6 +10,25 @@ class LocationsController < ApplicationController
   end
 
   def new
+    @location = Location.new
+    @activities = Activity.all
+    @activity = @location.activities.build
+  end
+
+  def create
+
+  end
+
+  def edit
+    @location = Location.find(params[:id])
+    @activities = Activity.all
+  end
+
+  def update
+
+  end
+
+  def destroy
 
   end
 end
