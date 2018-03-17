805302ce58513e96e929fb96d4a1e101bc65d25c
robert-laws <roblaws@me.com>
2018-03-17 20:46:46 +0300

    clean up profile welcome page and user account form

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,7 @@ class Address < ApplicationRecord
   belongs_to :user
 
   validates :street_1, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip_code, presence: true
 end
