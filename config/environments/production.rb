29633b0acc4b0edf6fbca4a77b96c5a6306be4b0
robert-laws <roblaws@me.com>
2018-03-18 20:55:56 +0300

    update produciton.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,7 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   config.serve_static_assets = true
+  config.serve_static_files = true
   config.assets.compile = true
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
