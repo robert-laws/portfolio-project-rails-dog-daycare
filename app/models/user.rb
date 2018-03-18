c4bcb0d5ec921c282d3c2efa11fbfb275527dd75
Robert Laws <roblaws@me.com>
2018-03-18 16:59:26 +0300

    add user validations, footer, and add to home page design

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,9 @@ class User < ApplicationRecord
   accepts_nested_attributes_for :addresses
   accepts_nested_attributes_for :dogs
 
-  validates :username, presence: true
-  validates :email, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :password, length: { minimum: 6 }
   # validates :first_name, presence: true
   # validates :last_name, presence: true
 
