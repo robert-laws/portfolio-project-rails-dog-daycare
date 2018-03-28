16719b688a45e548e17526130292755067109ee8
Robert Laws <roblaws@me.com>
2018-03-28 12:52:19 +0300

    fix bug for error hiding/showing edit functions only for admin users

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,7 @@ Reservation.destroy_all
 @location_3 = Location.create(name: "Your Dog Place", open_year: 2007, capacity: 34, size: 3250, city: "Milwalkee")
 @location_4 = Location.create(name: "Happy Pups", open_year: 2003, capacity: 16, size: 1600, city: "Cleveland")
 
-@activity_1 = Activity.create(name: "Fetch the Stick", description: "Sticks are thrown far and dogs run to retrieve them", best_for_type: "energetic", best_for_size: "big", energy_usage: "high")
+@activity_1 = Activity.create(name: "Fetch the Stick", description: "Sticks are thrown far and dogs run to retrieve them", best_for_type: "energetic", best_for_size: "large", energy_usage: "high")
 @activity_2 = Activity.create(name: "Solve the Puzzle", description: "Treats are available for the dog that can solve the puzzle", best_for_type: "smart", best_for_size: "medium", energy_usage: "low")
 @activity_3 = Activity.create(name: "Obstacle Course", description: "Dogs run through, over, and around a bunch of different obstacles", best_for_type: "agile", best_for_size: "medium", energy_usage: "high")
 @activity_4 = Activity.create(name: "Indoors Ball Bounce", description: "A bouncy ball is thrown indoors for the dog to chase and catch", best_for_type: "energetic", best_for_size: "small", energy_usage: "high")
